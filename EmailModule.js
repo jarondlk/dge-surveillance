fa71a5bb2e79799cd705c8fad4bf4776b37f3800
jarondlk <jaronchai78@gmail.com>
2022-09-30 16:12:39 +0700

    Add EmailModule

---
+++ EmailModule.js
@@ -0,0 +1,55 @@
+//don't forgor this -->"npm install nodemailer"
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'Suscurity_DGE03',
+        pass: 'SussyBaka69420'
+    }
+});
+//Photo Coding
+const photoMailOptions = {
+    from: 'tnidge03@gmail.com',
+    to: 'tnidge03@gmail.com',
+    subject: 'Intruder detected By our Sensors',//Email Header
+    html: '&lt;p> This Sussy person has entered our territory and try to do something sussy!! &lt;/p>',//Email Body
+    attachments: [{
+        filename: 'SusIntruderImage.jpg',
+        path: '----Insert Path--------' //create a folder to store the data e.g. "/home/pi/Node_Programs/photos/photo.jpg"
+    }]
+};
+
+module.exports.sendMailPhoto = function () {
+    transporter.sendMail(photoMailOptions, function (err, info) {
+        if (err) {
+            console.log(err.toString())
+        }
+        else {
+            console.log('Photo email success..!!');
+        }
+    });
+}
+//Video Coding
+const videoMailOptions = {
+    from: 'tnidge03@gmail.com',
+    to: 'tnidge03@gmail.com',
+    subject: 'Intruder detected By our Sensors',//Email Header
+    html: '&lt;p> This Sussy person has entered our territory and try to do something sussy!! &lt;/p>',//Email Body
+    attachments: [{
+        filename: 'SusIntruderVideo.h264',
+        path: '----Insert Path--------'//create a folder to store the data e.g. "/home/pi/Node_Programs/videos/video.h264"
+    }]
+};
+
+module.exports.sendMailVideo = function () {
+    transporter.sendMail(videoMailOptions, function (err, info) {
+        if (err) {
+            console.log(err.toString());
+        }
+        else {
+            console.log('Video email success..!!');
+        }
+    });
+}
+
