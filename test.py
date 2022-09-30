795cb78cc902624966a6aa7182f8e4badea66637
jarondlk <jaronchai78@gmail.com>
2022-09-30 19:26:35 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,45 @@
+from picamera import PiCamera
+from time import sleep
+import smtplib
+import time
+from datetime import datetime
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import RPi.GPIO as GPIO
+import time
+
+toaddr = 'tnidge03@gmail.com'
+me = 'tnidge03@gmail.com'
+Subject = 'security alert'
+
+GPIO.setmode(GPIO.BCM)
+
+P = PiCamera()
+P.resolution = (1024, 768)
+P.start_preview()
+
+GPIO.setup(23, GPIO.IN)
+while True:
+    if GPIO.input(23):
+        print("Motion...")
+        # camera warm-up time
+        time.sleep(2)
+        P.capture('movement.jpg')
+        time.sleep(10)
+        subject = 'Security allert!!'
+        msg = MIMEMultipart()
+        msg['Subject'] = subject
+        msg['From'] = me
+        msg['To'] = toaddr
+
+        fp = open('movement.jpg', 'rb')
+        img = MIMEImage(fp.read())
+        fp.close()
+        msg.attach(img)
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(user='tnidge03@gmail.com',
+                     password='poksix-jantif-8Tezbo')
+        server.send_message(msg)
+        server.quit()
