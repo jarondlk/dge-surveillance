64018dd5b8a97397f4d13dd85a31bbd436a81c77
jarondlk <jaronchai78@gmail.com>
2022-10-03 13:08:50 +0700

    Debug

--- test.py
+++ test.py
@@ -20,29 +20,28 @@ P.start_preview()
 
 GPIO.setup(23, GPIO.IN)
 while True:
-    if GPIO.input(23):
-        if (True):
-            print("Motion...")
-            # camera warm-up time
-            time.sleep(2)
-            P.capture('movement.jpg', use_video_port=True)
-            time.sleep(10)
-            subject = 'Security allert!!'
-            msg = MIMEMultipart()
-            msg['Subject'] = subject
-            msg['From'] = me
-            msg['To'] = toaddr
+    # if GPIO.input(23):
+    if (True):
+        print("Motion...")
+        # camera warm-up time
+        time.sleep(2)
+        P.capture('movement.jpg', use_video_port=True)
+        time.sleep(10)
+        subject = 'Security allert!!'
+        msg = MIMEMultipart()
+        msg['Subject'] = subject
+        msg['From'] = me
+        msg['To'] = toaddr
+        fp = open('movement.jpg', 'rb')
+        img = MIMEImage(fp.read())
+        fp.close()
+        msg.attach(img)
 
-            fp = open('movement.jpg', 'rb')
-            img = MIMEImage(fp.read())
-            fp.close()
-            msg.attach(img)
-
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.ehlo()
-            server.starttls()
-            server.ehlo()
-            server.login('tnidge03@gmail.com', 'fqhbrikxryrbadnl')
-            server.sendmail('tnidge03@gmail.com',
-                            'tnidge03@gmail.com', msg.as_string())
-            server.quit()
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login('tnidge03@gmail.com', 'fqhbrikxryrbadnl')
+        server.sendmail('tnidge03@gmail.com',
+                        'tnidge03@gmail.com', msg.as_string())
+        server.quit()
